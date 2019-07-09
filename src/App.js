2ee86210e5066be772cc2381042657adcb290be9
Simone Taeggi <startae14@gmail.com>
2019-07-09 16:54:46 +0200

    iterate a list of element and render them

--- src/App.js
+++ src/App.js
@@ -6,19 +6,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Simone'
+      monsters: [
+        {
+          name: 'Frankestein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'as1w'
+        }
+      ]
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello Simone Taeggi' })}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
       </div>
     )
   }
