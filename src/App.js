6166c36a839f2856c7f5e62e15cf006cbc1d91e8
Simone Taeggi <startae14@gmail.com>
2019-07-30 22:35:55 +0200

    added the card component and style

--- src/App.js
+++ src/App.js
@@ -34,13 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Yuah">
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))
-        }
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     )
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
-export const CardList = (props) => {
+export const CardList = ({ monsters }) => {
   return (
     <div className="card-list">
-      { props.children }
+    {
+      monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))
+    }
     </div>
   );
 };
