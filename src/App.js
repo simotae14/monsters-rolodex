8f6416840c815d612ebb1453e6e1687aeb7eb262
Simone Taeggi <startae14@gmail.com>
2019-07-31 22:24:21 +0200

    create the SearchBox Component with its style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -19,11 +20,15 @@ class App extends Component {
       }))
   }
 
+  handleOnChange = e => this.setState({searchField: e.target.value})
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => this.setState({searchField: e.target.value})} />
-        <CardList monsters={this.state.monsters} />
+        <SearchBox placeholder='search monsters' handleOnChange={this.handleOnChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
