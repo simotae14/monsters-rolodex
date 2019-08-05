54f1f7dddc43a920442ad36cd32e9f3900bab982
Simone Taeggi <startae14@gmail.com>
2019-08-05 22:30:19 +0200

    Updates

---
+++ precache-manifest.e70b77a72ce9c9b45f787422a6e804e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4158c75dae3c808da1f2f25c7dd2c846",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "625b369eec7fd8734b21",
+    "url": "/monsters-rolodex/static/css/main.2a38796b.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "625b369eec7fd8734b21",
+    "url": "/monsters-rolodex/static/js/main.35ae4bf6.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
