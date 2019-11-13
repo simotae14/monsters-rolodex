fc3eb6dd3d9d1bc000f7090b02f7c307717fd126
Simone Taeggi <startae14@gmail.com>
2019-11-13 20:00:13 +0100

    Updates

--- src/components/search-box/search-box.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './search-box.styles.css';
-
-export const SearchBox = ({placeholder, handleOnChange}) => {
-  return (
-    <input className="search" type='search' placeholder={placeholder} onChange={handleOnChange} />
-  );
-};
