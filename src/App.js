dc2e157c6b2ee441dc8882117ee831a376ed0c43
Simone <startae14@gmail.com>
2022-03-23 10:20:03 +0100

    Adding the state into the Class Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Simone'
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello my name is Simone
+            Hi {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change Name
+          </button>
         </header>
       </div>
     );
