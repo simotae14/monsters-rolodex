a87ac2eab4125255d0389676cdfe18f2effcbeca
Simone <startae14@gmail.com>
2022-03-23 10:58:03 +0100

    Example shallow merge inside the setState

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Simone'
+      name: {
+        firstName: 'Simone',
+        lastName: 'Taeggi',
+      },
+      company: 'eBay',
     };
   }
   render() {
@@ -16,12 +20,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
           <button onClick={() => {
             this.setState({
-              name: 'Rucola'
+              name: {
+                firstName: 'Rucola',
+                lastName: 'Taeggi',
+              }
             });
+            console.log(this.state);
           }}>
             Change Name
           </button>
