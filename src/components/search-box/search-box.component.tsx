1e821eaeac0de02e307d3093013c44e5a08eed65
Simone <startae14@gmail.com>
2022-05-04 19:53:13 +0200

    Using Types instead of interfaces in Functional Components

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -9,14 +9,43 @@ interface ISearchBoxProps {
   onChangeHandler: (a: string) => void
 }
 
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: (a: string) => void;
+};
+
+type CanadianAddress = {
+  street: string;
+  province: string;
+};
+
+type USAddress = {
+  street: string;
+  state: string;
+};
+
+type ItalianAddress = {
+  street: string;
+  region: string;
+}
+
+type Address = CanadianAddress | USAddress | ItalianAddress;
+
+const Address: Address = {
+  street: 'adas',
+  region: 'adasd',
+  state: 'ciao'
+};
+
 const SearchBox = ({
   onChangeHandler,
   className,
   placeholder,
-}: ISearchBoxProps) => {
+}: SearchBoxProps) => {
   return (
     <input
-      onChange={onChangeHandler}
+      onChange={(e) => onChangeHandler(e)}
       className={`search-box ${className}`}
       type='search'
       placeholder={placeholder}
