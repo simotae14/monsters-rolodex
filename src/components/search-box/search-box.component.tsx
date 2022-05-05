5d48caca6bda2185b57e4ca7a2da1cb3a8a1f121
Simone <startae14@gmail.com>
2022-05-05 15:19:29 +0200

    define the type of a change handler using ChangeEventHandler from react

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,41 +1,11 @@
+import { ChangeEventHandler } from 'react';
+
 import './search-box.styles.css';
-
-interface ISearchBoxProps {
-  className: string;
-  placeholder?: string;
-};
-
-interface ISearchBoxProps {
-  onChangeHandler: (a: string) => void
-}
 
 type SearchBoxProps = {
   className: string;
   placeholder?: string;
-  onChangeHandler: (a: string) => void;
-};
-
-type CanadianAddress = {
-  street: string;
-  province: string;
-};
-
-type USAddress = {
-  street: string;
-  state: string;
-};
-
-type ItalianAddress = {
-  street: string;
-  region: string;
-}
-
-type Address = CanadianAddress | USAddress | ItalianAddress;
-
-const Address: Address = {
-  street: 'adas',
-  region: 'adasd',
-  state: 'ciao'
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
 };
 
 const SearchBox = ({
@@ -45,7 +15,7 @@ const SearchBox = ({
 }: SearchBoxProps) => {
   return (
     <input
-      onChange={(e) => onChangeHandler(e)}
+      onChange={onChangeHandler}
       className={`search-box ${className}`}
       type='search'
       placeholder={placeholder}
@@ -54,6 +24,3 @@ const SearchBox = ({
 }
 
 export default SearchBox;
-const func: (a: string, b: number, c: boolean) => boolean = (a, b, c) => {
-  return true;
-}
